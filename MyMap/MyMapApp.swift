fb0ae1c9ffd992702e75982d682afc0d4c5765d3
tanaka <sisyamo2236@gmail.com>
2021-05-03 00:31:25 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by tanaka  on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
